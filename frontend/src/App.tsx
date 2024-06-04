cddcd365c30e7517d41718fca534e99bb1fc7e2e
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2024-06-04 15:26:08 -0400

    Favourites Functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,39 @@
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import "./App.css";
 import * as api from "./api";
 import { Recipe } from "./types";
 import RecipeCard from "./components/RecipeCard";
 import RecipeModal from "./components/RecipeModal";
+type Tabs = "search" | "favourites";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>(""); // Using <string> is a good pratice to define the type of the state
   const [recipes, setRecipes] = useState<Recipe[]>([]); //<Recipe[]> create a types of array
   const [selectedRecipe, setSelectedRecipe] = useState<Recipe | null>(null); // There might not be a selected recipe, so the type is Recipe | null
   const pageNumber = useRef(1); //useRef allow you to keep current value without re-rendering the component
+  const [selectedTab, setSelectedTab] = useState<Tabs>(); //SelectedTab can only be "search" or "favourites"
+  const [favouriteRecipes, setFavouriteRecipes] = useState<Recipe[]>([]); // The favourite recipes state is an array of Recipe objects
+
+  const addFavouriteRecipe = async (recipe: Recipe) => {
+    try {
+      await api.addFavouriteRecipes(recipe);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // Fetch favourite recipes when the app loads
+  useEffect(() => {
+    const fetchFavouriteRecipes = async () => {
+      try {
+        const favouriteRecipes = await api.getFavouriteRecipes();
+        setFavouriteRecipes(favouriteRecipes.results); // Update the state with the favourite recipes
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchFavouriteRecipes();
+  }, []);
 
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault(); // Prevent the default form submission behavior, which would cause a page reload
@@ -35,27 +59,52 @@ const App = () => {
 
   return (
     <div>
-      <form onSubmit={(event) => handleSearchSubmit(event)}>
-        <input
-          type="text"
-          required
-          placeholder="Enter a search term..."
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-        ></input>
-        <button type="submit">Submit</button>
-      </form>
-      {recipes.map((recipe) => (
-        <RecipeCard recipe={recipe} onClick={() => setSelectedRecipe(recipe)} />
-      ))}
+      <div className="tabs">
+        <h1 onClick={() => setSelectedTab("search")}> Recipe Search</h1>
+        <h1 onClick={() => setSelectedTab("favourites")}> Favourites </h1>
+      </div>
+      {/* Render the search tab only if the selected tab is "search" */}
+      {selectedTab === "search" && (
+        <>
+          <form onSubmit={(event) => handleSearchSubmit(event)}>
+            <input
+              type="text"
+              required
+              placeholder="Enter a search term..."
+              value={searchTerm}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            ></input>
+            <button type="submit">Submit</button>
+          </form>
+          {recipes.map((recipe) => (
+            <RecipeCard
+              recipe={recipe}
+              onClick={() => setSelectedRecipe(recipe)}
+            />
+          ))}
 
-      <button className="view-more-button" onClick={handleViewMoreClick}>
-        View More
-      </button>
-
+          <button className="view-more-button" onClick={handleViewMoreClick}>
+            View More
+          </button>
+        </>
+      )}
+      {/* Render the favourites tab only if the selected tab is "favourites" */}
+      {selectedTab === "favourites" && (
+        <div>
+          {favouriteRecipes.map((recipe) => (
+            <RecipeCard
+              recipe={recipe}
+              onClick={() => setSelectedRecipe(recipe)}
+            />
+          ))}
+        </div>
+      )}
       {/* Render the RecipeModal component only if there is a selected recipe */}
       {selectedRecipe ? (
-        <RecipeModal recipeId={selectedRecipe.id.toString()} onClose={() => setSelectedRecipe(null)} />
+        <RecipeModal
+          recipeId={selectedRecipe.id.toString()}
+          onClose={() => setSelectedRecipe(null)}
+        />
       ) : null}
     </div>
   );
--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,5 @@
+import { Recipe } from "./types";
+
 export const searchRecipes = async (searchTerm: string, page: number) => {
   const baseUrl = new URL("http://localhost:5001/api/recipes/search");
   baseUrl.searchParams.append("searchTerm", searchTerm);
@@ -23,4 +25,38 @@ export const getRecipeSummary = async (id: string) => {
   }
 
   return response.json();
+};
+
+export const getFavouriteRecipes = async () => {
+  const url = new URL("http://localhost:5001/api/recipes/favourite");
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(
+      "Failed to fetch favourite recipes during getFavouriteRecipes: " +
+        response.statusText
+    );
+  }
+
+  return response.json();
+};
+
+export const addFavouriteRecipes = async (recipe: Recipe) => {
+  const url = new URL("http://localhost:5001/api/recipes/favourite");
+  const body = {
+    recipeId: recipe.id,
+  };
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // Tell the server we are sending JSON
+    },
+    body: JSON.stringify(body), // Convert the body to a JSON string
+  });
+
+  if (!response.ok) {
+    throw new Error(
+      "Failed to add favourite recipe during addFavouriteRecipes: " +
+        response.statusText
+    );
+  }
 };
